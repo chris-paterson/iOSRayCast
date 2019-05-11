2387a24955ec1a5790db55d93716fa3dbb3a0759
Chris Paterson <cjpaterson.3@gmail.com>
2019-05-11 13:21:28 -0400

    Draw circle at point on tap.

--- RayCast2D/ViewController.swift
+++ RayCast2D/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let point = touch.location(in: view)
+        drawCircle(atPoint: point)
+    }
+    
+    fileprivate func drawCircle(atPoint: CGPoint) {
+        self.view.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
+        
+        let circlePath = UIBezierPath(arcCenter: atPoint, radius: CGFloat(8), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 3.0
+        
+        view.layer.addSublayer(shapeLayer)
+    }
 }
 
